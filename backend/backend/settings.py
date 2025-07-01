ca32872ac8b75b63163401eebec64c7c59bd82d5
Владимир Ли <vovalee@MBP-Vladimir.z24.guap.ru>
2025-07-01 09:16:54 +0300

    3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['123.123.123.123', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -72,7 +72,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 
 # Password validation
